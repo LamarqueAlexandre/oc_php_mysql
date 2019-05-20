562b97e6430ae8419cc087cdf8a414ac83c3c5ed
LamarqueAlexandre <lamarque.alexandre7@gmail.com>
2019-05-20 20:54:12 +0200

    Les sessions

--- mapage.php
+++ mapage.php
@@ -8,11 +8,6 @@
 // On démarre la session avant d'écrire du code HTML
 session_start();
 
-// On peut créer des variables de session dans $_SESSION
-$_SESSION['prenom'] = 'Alex';
-$_SESSION['nom'] = 'Lamarque';
-$_SESSION['age'] = '27';
-
 ?>
 
 <!DOCTYPE html>
@@ -21,12 +16,12 @@ $_SESSION['age'] = '27';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Titre de ma page</title>
+    <title>Ma page</title>
 </head>
 <body>
     <p>
-        Salut <?php echo $_SESSION['prenom']; ?> !<br />
-        Tu es à l'accueil de mon site (index.php). Tu veux aller sur une autre page ?
+        Re bonjour <?php echo $_SESSION['prenom']; ?> !<br />
+        Tu es sur ma page?
     </p>
     <p>
         <a href="mapage.php">Lien vers mapage.php</a>
